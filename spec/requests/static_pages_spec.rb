9962616bfd2db37beaa82ba30e64e6b5888a9295
Savannah Niles <savannahniles@gmail.com>
2013-07-13 19:27:59 -0500

    Finish user edit, update, a bunch of UI stuff for static pages home, contact, and about, and signin/signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,5 +1,6 @@
 require 'spec_helper'
 
+
 describe "Static pages" do
 
   subject { page }
@@ -7,7 +8,7 @@ describe "Static pages" do
   describe "Home page" do
     before { visit root_path }
 
-    it { should have_selector('h1',    text: 'Sample App') }
+    it { should have_selector('h1',    text: 'The news and articles you love, all in one place.') }
     it { should have_selector('title', text: full_title('')) }
     it { should_not have_selector 'title', text: '| Home' }
   end
